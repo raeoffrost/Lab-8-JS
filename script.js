80bb697a66c26b92ad70d1816a1ba9bcd1f7deed
raeoffrost <raeoffrost@gmail.com>
2024-11-26 23:35:54 -0600

    Added basic object display

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Part 1: Understanding and Creating Objects
+
+
 // Student object
-// students object array
-const students = {
+const student = {
     name: "Alice", 
     age: 85,
     enrolled: true,
-    courses: ["CPAN 101", "CPAN 102", "CPAN 103"]
+    courses: ["CPAN 101", "CPAN 102", "CPAN 103"],
+    basics() {
+       console.log(`Student is enrolled: ${this.enrolled}`);
+       console.log(`Student courses: ${this.courses}`);
+    }
 };
+
+console.log("Student name: " + student.name);
+console.log("Student age: " + student.age);
+console.log(student.basics());
