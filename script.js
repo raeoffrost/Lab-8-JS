65988ecd945cb501c5cc1a43d0b8d7afe4b7cfb6
raeoffrost <raeoffrost@gmail.com>
2024-11-26 23:46:59 -0600

    fixed undefined bug

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Part 1: Understanding and Creating Objects
-
+// // Part 1: Understanding and Creating Objects
 
 // Student object
 const student = {
     name: "Alice", 
     age: 85,
     enrolled: true,
-    courses: ["CPAN 101", "CPAN 102", "CPAN 103"],
-    basics() {
-       console.log(`Student is enrolled: ${this.enrolled}`);
-       console.log(`Student courses: ${this.courses}`);
-    }
+    courses: ["CPAN 101", "CPAN 102", "CPAN 103"]
 };
+student.basics = function() {
+    const basicInfo = `Student is enrolled: ${this.enrolled}\nStudent courses: ${this.courses}`;
+    console.log(basicInfo);
+}
 
 console.log("Student name: " + student.name);
 console.log("Student age: " + student.age);
-console.log(student.basics());
+student.basics();
